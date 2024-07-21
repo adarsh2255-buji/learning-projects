5c5b908e9b241ac92a4533ff0eab7a289dbbc3c1
adarsh <adarshsomanasrt@gmail.com>
2024-07-21 22:15:54 +0530

    updated code

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,23 +1,42 @@
 const inputTask = document.querySelector('.input-task');
 const btnAddTask = document.querySelector('.btn-add-task');
 const displayTask = document.querySelector('.display-task');
+const taskDueDate = document.querySelector('.task-due-date');
 
-const tasks = []
+const tasks = [];
 
+//ADD TASK
 function addTask(){
-    inputAddedTask = inputTask.value;
+    let inputAddedTask = inputTask.value;
+    let inputTaskDueDate = taskDueDate.value;
     inputTask.value = '';
-    tasks.push(inputAddedTask);
+    tasks.push({
+        taskName : inputAddedTask,
+        dueDate : inputTaskDueDate
+    });
     displayTasks()
-    console.log(tasks) 
 }
+
+//DISPLAY TASKS
 function displayTasks(){
     let taskList = '';
     for(let i=0; i<tasks.length; i++) {
         const todo = tasks[i];
-        const html = `<p>${todo}</p>`;
+        const {taskName, dueDate} = todo
+        const html = `
+        <li class="task-list">
+        <span>${taskName}</span>
+        <span> ${dueDate}</span>
+        <button class="btn-delete"
+        onclick = "tasks.splice(${i}, 1);
+        displayTasks(); "
+        >delete</button>
+        </li>`;
         taskList += html
     }
     displayTask.innerHTML = taskList;
 }
+
+
+// ADDEVENTLISTNERS
 btnAddTask.addEventListener('click',addTask);
